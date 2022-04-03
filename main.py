89b55407349ab0e1cfd04d0078f857b67b6ea9b4
N3dal <nedalxzo@gmail.com>
2022-04-03 15:18:44 +0200

    create num2written function

--- main.py
+++ main.py
@@ -17,6 +17,11 @@
 # 21=> "twenty-one" not "twenty one",
 # 771=> "seven hundred and seventy-one" not "seven hundred and seventy-one"
 
+# TODO: make it this script convert from binary/hex/oct to decimal then,
+# TODO: write the written number, in simple words convert from (binary/hex/oct) to written num.
+# TODO: convert any exp number to written for example "3e10" express on it first then write it.
+# TODO: or "3.5E10" same thing.
+
 
 from os import name as OS_NAME
 from os import system
@@ -35,6 +40,19 @@ def clear():
 
 
 clear()
+
+
+def num2written(number: object):
+    """convert any float/int/string number to written number.
+    input-type: float/int/str.
+    valid inputs:
+    '12345' => [VALID]
+    12345 => [VALID]
+    123.45 => [VALID]
+    '32FD' => [NON-VALID]
+
+    output-type: string.
+    """
 
 
 def main():
